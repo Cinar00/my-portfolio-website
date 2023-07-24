333ee5afedc13b0936687eebbd450961202f417d
Beytullah Cinar <beytullah.cinar@brands-a.com>
2023-07-24 16:42:40 +0300

    updated

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -21,7 +21,15 @@ const Skills = () => {
       <h2 className="font-bold text-8xl mt-64 text-center md:text-6xl md:mt-28">
         Skills
       </h2>
-      <div className="w-full h-screen relative flex items-center justify-center rounded-full bg-circularLight dark:bg-circularDark lg:h-[80vh] sm:h-[60vh] xs:h-[50vh] lg:bg-circularLightLg lg:dark:bg-circularDarkLg">
+      <div
+        className="w-full h-screen relative flex items-center justify-center rounded-full bg-circularLight dark:bg-circularDark lg:h-[80vh] sm:h-[60vh] xs:h-[50vh] 
+      
+      lg:bg-circularLightLg lg:dark:bg-circularDarkLg
+      md:bg-circularLightMd md:dark:bg-circularDarkMd
+      sm:bg-circularLightSm sm:dark:bg-circularDarkSm
+      
+      "
+      >
         <motion.div
           className="flex items-center justify-center rounded-full font-semibold bg-dark text-light dark:bg-light dark:text-dark dark:shadow-light p-8 shadow-dark cursor-pointer lg:p-6 md:p-4 xs:p-2 xs:text-xs"
           whileHover={{ scale: 1.05 }}
--- src/pages/about.js
+++ src/pages/about.js
@@ -8,6 +8,7 @@ import { useInView, useMotionValue, useSpring } from "framer-motion";
 import Skills from "@/components/Skills";
 import Experience from "@/components/Experience";
 import Education from "@/components/Education";
+import TransitionEffect from "@/components/TransitionEffect";
 
 const AnimatedNumbers = ({ value }) => {
   const ref = useRef(null);
@@ -40,6 +41,7 @@ const about = () => {
         <title>Çınar | About Page</title>
         <meta name="description" content="about me" />
       </Head>
+      <TransitionEffect />
       <main className="flex w-full flex-col items-center justify-center dark:text-light">
         <Layout className="pt-16">
           <AnimatedText
@@ -52,19 +54,19 @@ const about = () => {
                 Biography
               </h2>
               <p className="font-medium">
-                Hi, I'm Çınar, a web developer and UI/UX designer with a passion
+                Hi, I m Çınar, a web developer and UI/UX designer with a passion
                 for creating beautiful, functional, and user-centered digital
                 experiences. With 4 years of experience in the field. I am
-                always looking for new and innovative ways to bring my clients'
+                always looking for new and innovative ways to bring my clients
                 visions to life.
               </p>
               <p className="my-4 font-medium">
                 I believe that design is about more than just making things look
-                pretty – it's about solving problems and creating intuitive,
+                pretty – it s about solving problems and creating intuitive,
                 enjoyable experiences for users.
               </p>
               <p className="font-medium">
-                Whether I'm working on a website, mobile app, or other digital
+                Whether I m working on a website, mobile app, or other digital
                 product, I bring my commitment to design excellence and
                 user-centered thinking to every project I work on. I look
                 forward to the opportunity to bring my skills and passion to
